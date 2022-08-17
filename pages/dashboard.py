420cd417fcc49d52cb948ab316ce46a985e33e91
IuliiaBespalova <110619095+IuliiaBespalova@users.noreply.github.com>
2022-08-17 09:45:54 +0300

    task2

---
+++ pages/dashboard.py
@@ -0,0 +1,18 @@
+from pages.base_page import BasePage
+
+
+class Dashboard(BasePage):
+    sigh_out_button_xpath = //*[text()='Sign out']
+    logo_scouts_panel_xpath = //*[@title='Logo Scouts Panel']
+    scouts_panel_header_xpath = //header/div/h6
+    search_field_xpath = //div/input
+    input_placeholder_xpath = //*[@class="MuiInputBase-input jss174"]
+    dev_team_contact_xpath = //a[contains(@href, '://')]
+    login_field_xpath = // *[text()="Login"]
+    login_field_xpath = // *[@ id="login"]
+
+
+
+
+
+    pass
--- pages/login_page.py
+++ pages/login_page.py
@@ -3,8 +3,13 @@
 #
 # class LoginPage(BasePage):
 #     login_field_xpath = "//*[@id='login']"
+<<<<<<< Updated upstream
 #     password_field_xpath = "//[@id='password']"
 #     sign_in_button_xpath = "//[@id='sign_in_button']"
+=======
+#     password_field_xpath = "//*[@id='password']"
+#     sign_in_button_xpath = "//*[@id='sign_in_button']"
+>>>>>>> Stashed changes
 #
 #     def type_in_email(self, email):
 #         self.field_send_keys(self.login_field_xpath, email)
